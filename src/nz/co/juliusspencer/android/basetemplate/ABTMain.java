8a2219b77c72eba5b7ff123169d9269c250615b7
Julius Spencer <jspencer@juliusspencer.co.nz>
2010-10-18 10:31:32 +1300

    Update for Main Activity

--- src/nz/co/juliusspencer/android/basetemplate/ABTMain.java
+++ src/nz/co/juliusspencer/android/basetemplate/ABTMain.java
@@ -1,13 +1,52 @@
 package nz.co.juliusspencer.android.basetemplate;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
 
 public class ABTMain extends Activity {
-    /** Called when the activity is first created. */
+
+	
+	private static final int MENU_SETTINGS_ID = Menu.FIRST;
+
+	private static final int VIEW_PREFS = -1;
+	
+
+	//  -------------------------------------------
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+	//  -------------------------------------------
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add(Menu.NONE, MENU_SETTINGS_ID, Menu.NONE, R.string.settings).setIcon(android.R.drawable.ic_menu_preferences);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	//  -------------------------------------------
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+
+		Intent i = null;
+		switch(item.getItemId()) {
+		case MENU_SETTINGS_ID:
+			i = new Intent(this, ABTPreferences.class);
+			startActivityForResult(i, VIEW_PREFS);
+			return true;
+		}
+
+		return super.onMenuItemSelected(featureId, item);
+	}
+	
+	
+	//  -------------------------------------------
+
 }
